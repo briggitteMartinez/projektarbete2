1698d0a325ea43e7ce4fb3804e8ecbf364a54675
Esmeralda Cruz <esmecb0@gmail.com>
2025-01-12 17:54:18 +0100

    min commit

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const Datastore = require('slqite3').verbose();
+const Datastore = require('better-sqlite3');
 const db = new Datastore('./db/products.db');
 
 router.get('/', function(req, res, next) {});
 
 module.exports = router;
+
+prompt("hej")
