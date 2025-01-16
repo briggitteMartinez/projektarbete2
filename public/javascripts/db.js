c527244f2a862dfb6bb829e1fc0e3ef05b101ad8
Erik Högberg <erik-hogberg@hotmail.com>
2025-01-16 10:42:02 +0100

    make search bar

---
+++ public/javascripts/db.js
@@ -0,0 +1,16 @@
+const Database = require('better-sqlite3');
+const db = new Database('./db/products.db', { verbose: console.log });
+
+function searchProducts(query, callback) {
+  try {
+    const stmt = db.prepare('SELECT * FROM productDetails WHERE name LIKE ? OR description LIKE ?');
+    const results = stmt.all(`%${query}%`, `%${query}%`);
+    callback(null, results);
+  } catch (err) {
+    callback(err);
+  }
+}
+
+module.exports = {
+  searchProducts: searchProducts
+};
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../public/javascripts/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'USER' });
+});
+
+router.get('/search', function(req, res, next) {
+  var query = req.query.q;
+  db.searchProducts(query, function(err, products) {
+    if (err) {
+      return next(err);
+    }
+    res.render('user/search-results', { title: 'Search Results', products: products });
+  });
 });
 
 module.exports = router;
