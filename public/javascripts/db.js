16466d65505f911b28b9d21029693cd21d2e2395
Erik Högberg <erik-hogberg@hotmail.com>
2025-01-20 12:47:12 +0100

    expanded search functionality, fixed minor bugs in header and footer, added url slug functionality to details page and added basic search result list

--- public/javascripts/db.js
+++ public/javascripts/db.js
@@ -3,14 +3,13 @@ const db = new Database('./db/products.db', { verbose: console.log });
 
 function searchProducts(query, callback) {
   try {
-    const stmt = db.prepare('SELECT * FROM productDetails WHERE name LIKE ? OR description LIKE ?');
-    const results = stmt.all(`%${query}%`, `%${query}%`);
+    const stmt = db.prepare('SELECT * FROM productDetails WHERE name LIKE ? OR description LIKE ? OR category LIKE ? OR brand LIKE ? OR color LIKE ?');
+    const results = stmt.all(`%${query}%`, `%${query}%`, `${query}%`, `%${query}%`, `%${query}%`);
     callback(null, results);
   } catch (err) {
     callback(err);
   }
 }
-
 
 module.exports = {
   db: db,
