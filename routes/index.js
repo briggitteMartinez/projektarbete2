16a379a21bcf7e459618eabb5f6e7678813c98e5
Vicci <vicci.lidback@gmail.com>
2025-01-29 23:13:47 +0100

    Update index page and new sale page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var db = require("../public/javascripts/db");
+
+//prepare the database
+const Database = require("better-sqlite3");
+const data = new Database("./db/products.db", { verbose: console.log });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res) {
+  const rows = data
+    .prepare("SELECT * FROM productDetails ORDER BY RANDOM() LIMIT 6")
+    .all();
+
+  // Här lägger vi till JSON.parse för att konvertera image-strängen till en array
+  rows.forEach((product) => {
+    if (product.image) {
+      product.image = JSON.parse(product.image); // Omvandla image från JSON-sträng till en array
+    }
+  });
+
+  console.log("Produkterna:", JSON.stringify(rows, null, 2)); // Kontrollera datan
+
+  res.render("index", { products: rows });
 });
 
 module.exports = router;
